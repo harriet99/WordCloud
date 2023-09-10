f4e01618d9756fd591730a1438df76c8df7069b7
Harriet Kim <harriet@Harriets-MacBook-Pro.local>
2023-09-09 22:15:57 -0400

    initialize react project

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            // <Router>
+            //     <AppShell>
+            //         <div>
+            //             <Route exact path="/" component={Home}/>
+            //             <Route exact path="/texts" component={Texts}/>
+            //             <Route exact path="/words" component={Words}/>
+            //             <Route exact path="/detail/:textID" component={Detail}/>
+            //         </div>
+            //     </AppShell>
+            // </Router>
+            <div>
+                <h3>
+                    Hello World
+                </h3>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+// const theme = createMuiTheme({
+//     typography: {
+//         useNextVariants: true,
+//         fontFamily: "Noto Sans KR"
+//     }
+// });
+
+// ReactDOM.render(<MuiThemeProvider theme={theme}><App/></MuiThemeProvider>, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
